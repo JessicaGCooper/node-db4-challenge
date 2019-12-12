5fcec6ea0c0a9550babb67abc0e4c1385585c083
Jessica Cooper <jessicagcooper@gmail.com>
2019-12-12 16:24:04 -0600

    Set up file structure and basic files needed.

---
+++ recipes/recipes-model.js
@@ -0,0 +1,5 @@
+const db = require("../data/db-Config.js")
+
+module.exports = {
+   //list helper functions here
+}
---
+++ recipes/recipes-router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const Recipes = require('./recipes-model.js');
+
+const router = express.Router();
+
+
+//build http requests here
+
+module.exports = router;
