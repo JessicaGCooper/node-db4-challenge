00213fab9c550f193f1189173e1034b8a78e3e88
Jessica Cooper <jessicagcooper@gmail.com>
2019-12-13 01:06:47 -0600

    Wrote helper functions for MVP and HTTP requests for functions & tested in insomnia.

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -6,5 +6,47 @@ const router = express.Router();
 
 
 //build http requests here
+router.get('/', (req, res) => {
+    Recipes.getRecipes()
+    .then(recipes => {
+      res.json(recipes);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get recipes.', err });
+    });
+  });
+  
+  router.get('/:id/shoppinglist', (req, res) => {
+    const recipe_id = req.params.id;
+  
+    Recipes.getShoppingList(recipe_id)
+    .then(list => {
+      if (list.length) {
+        res.json(list);
+      } else {
+        res.status(404).json({ message: 'Could not find shopping list for given recipe.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get shopping list.', err });
+    });
+  });
+
+  router.get('/:id/instructions', (req, res) => {
+    const recipe_id = req.params.id;
+  
+    Recipes.getInstructions(recipe_id)
+    .then(instructions => {
+      if (instructions.length) {
+        res.json(instructions);
+      } else {
+        res.status(404).json({ message: 'Could not find instructions for given recipe.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get recipe instructions.', err });
+    });
+  });
+
 
 module.exports = router;
